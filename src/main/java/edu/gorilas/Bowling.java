9bd00f3c7f329f112e0497eb6ec42e89b63d0c22
b4391co <b4391co@proton.me>
2024-01-26 18:28:15 +0100

    refactor(Frame): Constructor, getters

--- src/main/java/edu/gorilas/Bowling.java
+++ src/main/java/edu/gorilas/Bowling.java
@@ -9,6 +9,5 @@ public class Bowling
     public static void main( String[] args ) {
         ScoreCard s1 = new ScoreCard();
 
-        s1.calculateScore("12345123451234512345");
     }
 }
--- src/main/java/edu/gorilas/Frame.java
+++ src/main/java/edu/gorilas/Frame.java
@@ -1,13 +1,21 @@
 package edu.gorilas;
 
-import java.util.List;
-
 public class Frame {
-    private Pin pin1;
-    private Pin pin2;
-    private int score;
+    private int pin1;
+    private int pin2;
+    static int score = 0;
+    
+    public Frame() {
+    }
 
+    public Frame(int pin1, int pin2) {
+        this.pin1 = pin1;
+        this.pin2 = pin2;
+        this.score += pin1 + pin2;
+    }
 
-
+    public int getScore() {
+        return score;
+    }
 
 }
