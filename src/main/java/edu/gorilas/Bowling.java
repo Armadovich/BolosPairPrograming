7523eab2ebadb8648d8c7a3b2c13d1716fa004aa
b4391co <b4391co@proton.me>
2024-01-29 21:28:46 +0100

    refactor(ScoreCard): funcion de Strike y Spare

--- src/main/java/edu/gorilas/Bowling.java
+++ src/main/java/edu/gorilas/Bowling.java
@@ -8,6 +8,8 @@ public class Bowling
 {
     public static void main( String[] args ) {
         ScoreCard s1 = new ScoreCard();
-
+        ScoreCard s2 = new ScoreCard();
+        //System.out.println(s1.calculateScore("12345123451234512345"));
+        System.out.println(s2.calculateScore("XXXXXXXXXXXX"));
     }
 }
